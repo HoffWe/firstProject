c355a6316315e4d96e034b271ede1b14b027666c
Axk23 <Adriko.2003@gmail.com>
2025-10-22 21:04:49 +0200

    Dodanie konstr. setterow i getterow do Flight

--- src/Models/Flight.java
+++ src/Models/Flight.java
@@ -12,4 +12,91 @@ public class Flight {
     private int duration;
     private int gate;
     private int stand;
+
+
+    //konstruktor
+
+    public Flight(int idFlight, Airplane airplane, String destination, String departure, LocalDateTime departureTime, LocalDateTime arrivalTime,
+                  int gate, int stand) {
+
+        this.id = idFlight;
+        this.airplane = airplane;
+        this.destination = destination;
+        this.departure = departure;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+        this.gate = gate;
+        this.stand = stand;
+
+    }
+
+    //gettery
+
+    public int getId(){
+        return id;
+    }
+
+    public Airplane getAirplane(){
+        return airplane;
+    }
+
+    public String getDestination(){
+        return destination;
+    }
+
+    public String getDeparture(){
+        return departure;
+    }
+
+    public LocalDateTime getDepartureTime(){
+        return departureTime;
+    }
+
+    public LocalDateTime getArrivalTime(){
+        return arrivalTime;
+    }
+
+    public int getGate(){
+        return gate;
+    }
+
+    public int getStand(){
+        return stand;
+    }
+
+
+    //settery
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public void setAirplane(Airplane airplane){
+        this.airplane = airplane;
+    }
+
+    public void setDestination(String destination){
+        this.destination = destination;
+    }
+
+    public void setDeparture(String departure){
+        this.departure = departure;
+    }
+
+    public void setArrivalTime(LocalDateTime arrivalTime){
+        this.arrivalTime = arrivalTime;
+    }
+
+    public void setDepartureTime(LocalDateTime departureTime){
+        this.departureTime = departureTime;
+    }
+
+    public void setGate(int gate){
+        this.gate = gate;
+    }
+
+    public void setStand(int stand){
+        this.stand = stand;
+    }
+
 }
